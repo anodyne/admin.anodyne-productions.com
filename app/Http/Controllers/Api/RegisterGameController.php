effa47c0f17bf4323b146939cd3d893584943bf5
David VanScott <david.vanscott@gmail.com>
2023-01-07 14:05:30 -0500

    Updates for game registration

--- app/Http/Controllers/Api/RegisterGameController.php
+++ app/Http/Controllers/Api/RegisterGameController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Game;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class RegisterGameController extends Controller
@@ -15,7 +16,7 @@ class RegisterGameController extends Controller
         $this->middleware(function ($request, $next) {
             $url = str($request->url);
 
-            if ($url->contains(['localhost', '127.0.0.1', '.test'])) {
+            if ($url->contains(config('anodyne.registration.exceptions'))) {
                 return false;
             }
 
@@ -25,18 +26,21 @@ class RegisterGameController extends Controller
 
     public function __invoke(Request $request)
     {
-        Game::updateOrCreate(
-            ['url' => $request->url],
-            [
-                'name' => $request->name,
-                'genre' => $request->genre,
-                'version' => $request->version,
-                'php_version' => $request->php_version,
-                'db_driver' => $request->db_driver,
-                'db_version' => $request->db_version,
-                'server_software' => $request->server_software,
-            ]
-        );
+        $data = [
+            'name' => $request->name,
+            'genre' => $request->genre,
+            'version' => $request->version,
+            'php_version' => $request->php_version,
+            'db_driver' => $request->db_driver,
+            'db_version' => $request->db_version,
+            'server_software' => $request->server_software,
+        ];
+
+        if (! empty($request->get('install_date'))) {
+            $data['created_at'] = Carbon::createFromFormat('U', $request->get('install_date'));
+        }
+
+        Game::updateOrCreate(['url' => $request->url], $data);
 
         return response()->json();
     }
